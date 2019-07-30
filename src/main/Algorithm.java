3ac94d5005e7dded46e392538b81ecb51597d745
andy0926 <tanandy2011@hotmail.com>
2019-07-30 18:08:25 +0800

    Error detection havent fix

--- src/main/Algorithm.java
+++ src/main/Algorithm.java
@@ -10,7 +10,7 @@ public class Algorithm {
 		public int compare(Object o1, Object o2) {
 			CompareData j1 = (CompareData) o1;
 			CompareData j2 = (CompareData) o2;
-			System.out.println(o1);
+			
 			
 			if (j1.profit != j2.profit) {
 				System.out.println("aT COMPARABLE");
@@ -18,7 +18,7 @@ public class Algorithm {
 			}
 
 			else {
-				System.out.println("aT COMPARABLE");
+				System.out.println("aT COMPARABLE2222222222222");
 				return j2.deadline - j1.deadline;
 			}
 
@@ -30,25 +30,30 @@ public class Algorithm {
 	public void printJobScheduling(Data data[], int n) {
 		// Creating object of Sorted class
 		
-		
+		int counter =0;
 		CompareData[] cd = new CompareData[n];
 		/////////////////////////////////////////////
 		for(int x = 0;x<n;x++) {
+			if(data[x]!=null) {
+				System.out.println("at the for loop");
+				cd[x] = new CompareData(data[x].id, data[x].deadline,data[x].profit );
+				counter++;
+			}
 			
-			cd[x] = new CompareData(data[x].id, data[x].deadline,data[x].profit );
 			
 		}
 		Sorted sorter = new Sorted();
+		/////
 		System.out.println("sort");
 		Arrays.sort(cd, sorter);
 		
 		// Creating TreeSet Object
 		TreeSet<Integer> ts = new TreeSet<>();
 
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < counter; i++)
 			ts.add(i);
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < counter; i++) {
 			Integer x = ts.floor(data[i].deadline - 1);
 
 			if (x != null) {
@@ -62,7 +67,8 @@ public class Algorithm {
 
 	public Data[] ValidateDeadline(Data data[], Data newData[], int n, int newN) {
 		if (n <= data.length - 1) {
-			if (data[n].endTime % 100 > data[n].deadline) {
+			System.out.println("Divided End Time: "+data[n].endTime / 100);
+			if (data[n].endTime / 100 > data[n].deadline) {
 				// Error Checking
 				// End Time more than deadline
 				// Jump to next object and move on the next one
@@ -70,9 +76,9 @@ public class Algorithm {
 				System.out.println("Here Wrong");
 				return ValidateDeadline(data, newData, n + 1, newN);
 				
-			} else if (data[n].endTime % 100 <= data[n].deadline) {
+			} else if (data[n].endTime / 100 <= data[n].deadline) {
 				newData[newN] = data[n];
-				System.out.println(data[n].endTime);
+				System.out.println(newData[newN].endTime);
 				System.out.println("Here Wrong!!!!");
 				return ValidateDeadline(data, newData, n + 1, n + 1);
 			}
@@ -80,8 +86,8 @@ public class Algorithm {
 			// Exit the this function
 			return newData;
 		} else {
-			System.out.println(newData);
-			System.out.println("Out the recursion");
+			System.out.println("Out the recursion    "+ newData.length );
+			
 			return newData;
 		}
 
--- src/main/Test.java
+++ src/main/Test.java
@@ -8,13 +8,15 @@ public class Test {
 		int n =0, newN =0;
 		Data[] data = new Data[numberOb];
 		Data[] newData = new Data[numberOb];
-		Data[] finalData = new Data[numberOb];
 		Algorithm algo = new Algorithm();
-        data[0] = new Data('a', 2, 100, 1000, 1000); 
-        data[1] = new Data('b', 1, 19, 900, 1000); 
-        data[2] = new Data('c', 2, 27, 1400, 1600); 
-        data[3] = new Data('d', 1, 25, 1700, 1800); 
-        data[4] = new Data('e', 3, 15, 1900, 2000); 
+        
+		//id deadline profit
+		data[0] = new Data('a', 1, 100, 1000, 100); 
+        data[1] = new Data('b', 1, 19, 900, 100); 
+        data[2] = new Data('c', 2, 27, 1400, 200); 
+        data[3] = new Data('d', 8, 25, 1700, 800); 
+        data[4] = new Data('e', 4, 15, 1900, 300); 
+        
 		System.out.println(".........");
 		algo.printJobScheduling(algo.ValidateDeadline(data, newData, n, newN), numberOb);
         
