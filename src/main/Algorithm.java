909c71848a766c027445f2cdc8e4ab1f71a8bec6
andy0926 <tanandy2011@hotmail.com>
2019-07-30 16:50:47 +0800

    Syntax Error fixed

--- src/main/Algorithm.java
+++ src/main/Algorithm.java
@@ -4,62 +4,86 @@ import java.util.Comparator;
 import java.util.*;
 
 public class Algorithm {
-    public class Sorted implements Comparator {
+	public class Sorted implements Comparator {
 
-        @Override
-        public int compare(Object o1, Object o2) {
-            Data j1 = (Data) o1;
-            Data j2 = (Data) o2;
+		@Override
+		public int compare(Object o1, Object o2) {
+			CompareData j1 = (CompareData) o1;
+			CompareData j2 = (CompareData) o2;
+			System.out.println(o1);
+			
+			if (j1.profit != j2.profit) {
+				System.out.println("aT COMPARABLE");
+				return j2.profit - j1.profit;
+			}
 
-            if (j1.profit != j2.profit)
-                return j2.profit - j1.profit;
-            else
-                return j2.deadline - j1.deadline;
-        }
+			else {
+				System.out.println("aT COMPARABLE");
+				return j2.deadline - j1.deadline;
+			}
 
-    }
+		}
 
-    // Function to print job scheduling
-    public void printJobScheduling(Data data[], int n) {
-        // Creating object of Sorted class
-        Sorted sorter = new Sorted();
+	}
 
-        Arrays.sort(data, sorter);
+	// Function to print job scheduling
+	public void printJobScheduling(Data data[], int n) {
+		// Creating object of Sorted class
+		
+		
+		CompareData[] cd = new CompareData[n];
+		/////////////////////////////////////////////
+		for(int x = 0;x<n;x++) {
+			
+			cd[x] = new CompareData(data[x].id, data[x].deadline,data[x].profit );
+			
+		}
+		Sorted sorter = new Sorted();
+		System.out.println("sort");
+		Arrays.sort(cd, sorter);
+		
+		// Creating TreeSet Object
+		TreeSet<Integer> ts = new TreeSet<>();
 
-        // Creating TreeSet Object
-        TreeSet<Integer> ts = new TreeSet<>();
+		for (int i = 0; i < n; i++)
+			ts.add(i);
 
-        for (int i = 0; i < n; i++)
-            ts.add(i);
+		for (int i = 0; i < n; i++) {
+			Integer x = ts.floor(data[i].deadline - 1);
 
-        for (int i = 0; i < n; i++) {
-            Integer x = ts.floor(data[i].deadline - 1);
+			if (x != null) {
+				System.out.print(data[i].id + " ");
+				ts.remove(x);
+			}
+		}
+	}
 
-            if (x != null) {
-                System.out.print(data[i].id + " ");
-                ts.remove(x);
-            }
-        }
-    }
+	// Recursion to validate the data
 
-    // Recursion to validate the data
+	public Data[] ValidateDeadline(Data data[], Data newData[], int n, int newN) {
+		if (n <= data.length - 1) {
+			if (data[n].endTime % 100 > data[n].deadline) {
+				// Error Checking
+				// End Time more than deadline
+				// Jump to next object and move on the next one
+				
+				System.out.println("Here Wrong");
+				return ValidateDeadline(data, newData, n + 1, newN);
+				
+			} else if (data[n].endTime % 100 <= data[n].deadline) {
+				newData[newN] = data[n];
+				System.out.println(data[n].endTime);
+				System.out.println("Here Wrong!!!!");
+				return ValidateDeadline(data, newData, n + 1, n + 1);
+			}
+			// Go to next function
+			// Exit the this function
+			return newData;
+		} else {
+			System.out.println(newData);
+			System.out.println("Out the recursion");
+			return newData;
+		}
 
-    public Data[] ValidateDeadline(Data data[], Data newData[], int n, int newN) {
-        if (n <= data.length) {
-            if (data[n].endTime > data[n].deadline) {
-                // Error Checking
-                // End Time more than deadline
-                // Jump to next object and move on the next one
-                return ValidateDeadline(data, newData, n + 1, newN);
-            } else if (data[n].endTime < data[n].deadline) {
-                data[n] = newData[newN];
-                return ValidateDeadline(data, newData, n + 1, n + 1);
-            }
-            // Go to next function
-            // Exit the this function
-            return data;
-        }
-        else
-        	return data;
-    }
+	}
 }
---
+++ src/main/CompareData.java
@@ -0,0 +1,18 @@
+package main;
+
+public class CompareData {
+	char id;
+	int deadline;
+	int profit;
+
+
+	// Constructor
+	CompareData(char id, int deadline, int profit) 
+	    { 
+	        this.id = id; 
+	        this.deadline = deadline; 
+	        this.profit = profit; 
+	    }
+
+
+}
