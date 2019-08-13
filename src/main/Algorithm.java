eed903c64939aac5af2ecf6ac02dc06ad5a25673
andy0926 <tanandy2011@hotmail.com>
2019-08-14 01:12:32 +0800

    Deadline is arranged

--- src/main/Algorithm.java
+++ src/main/Algorithm.java
@@ -14,11 +14,7 @@ public class Algorithm {
 			// System.out.println("compare data 1 profit :"+ j1.profit );
 
 			if (j1.profit != j2.profit) {
-				// System.out.print("j1 profit: ");
-				// System.out.println(j1.profit);
-				// System.out.print("j2 profit: ");
-				// System.out.println(j2.profit);
-				// System.out.print("@@@@");
+
 				// System.out.println(j2.profit - j1.profit);
 				return j2.profit - j1.profit;
 			}
@@ -30,47 +26,81 @@ public class Algorithm {
 		}
 	}
 
-	public void printJobScheduling(CompareData[] compareData, int nObject, Data[] data) {
+	public class SortDeadline implements Comparator<Object> {
+
+		@Override
+		public int compare(Object o1, Object o2) {
+			CompareData j1 = (CompareData) o1;
+			CompareData j2 = (CompareData) o2;
+
+			// System.out.println("compare data 1 profit :"+ j1.profit );
+
+			if (j1.deadline != j2.deadline) {
+
+				// System.out.println(j2.profit - j1.profit);
+				return j2.deadline - j1.deadline;
+			}
+
+			else {
+				// System.out.println("aT COMPARABLE2222222222222");
+				return j2.profit - j1.profit;
+			}
+		}
+	}
+
+	public void MaxProfitJobList(CompareData[] compareData, Data[] data, CompareData[] arrangedCompareData) {
+		int counter = 0;
 		// Creating object of Sorted class
 
-		//Check the data
-		// for (int z = 0; z < data.length; z++) {
-
-		// 	System.out.print(data[z].id + "||" + data[z].deadline + "||" + data[z].profit + "||" + data[z].startTime
-		// 			+ "||" + data[z].endTime);
-		// 	System.out.println();
-		// 	System.out.println("====================================================");
-		// 	System.out.print(compareData[z].id + "||" + compareData[z].deadline + "||" + compareData[z].profit);
-		// 	System.out.println();
-		// 	System.out.println("####################################################");
-		// }
 
 		Sorted sorter = new Sorted();
-
+		SortDeadline sortDeadline = new SortDeadline();
 		// Sort the cd.profit from highest to lowest
 		Arrays.sort(compareData, sorter);
+
+		// List out the compareData which is sorted
+		// for (int i = 0; i < nObject; i++) {
+		// System.out.println(compareData[i].id + "@" + compareData[i].deadline + "@" +
+		// compareData[i].profit);
+		// }
+		// System.out.println("==========================================");
 
 		// Creating TreeSet Object
 		TreeSet<Integer> ts = new TreeSet<>();
 
-		for (int i = 0; i < nObject; i++)
+		for (int i = 0; i < data.length; i++)
 			ts.add(i);
 
-		for (int i = 0; i < nObject; i++) {
+		for (int i = 0; i < data.length; i++) {
 			Integer x = ts.floor(compareData[i].deadline - 1);
 
 			if (x != null) {
-				System.out.print(compareData[i].id + " ");
-
-				for (int z = 0; z < data.length; z++) {
-					if (data[z].id == compareData[i].id)
-						System.out.print(data[z].deadline + "          " + data[z].profit + "  " + data[z].startTime
-								+ "       " + data[z].endTime);
-
-				}
-				System.out.println();
+				// System.out.print(compareData[i].id + " ");
+				arrangedCompareData[counter++] = compareData[i];
+				// for (int z = 0; z < data.length; z++) {
+				// 	if (data[z].id == compareData[i].id) {
+				// 		System.out.print(data[z].deadline + " " + data[z].profit + " " +
+				// 		data[z].startTime
+				// 		+ " " + data[z].endTime);
+						
+				// 	}
+				// }
+				// System.out.println();
 				ts.remove(x);
 			}
 		}
+
+		// Sort the Deadline from highest to lowest
+		Arrays.sort(arrangedCompareData, sortDeadline);
+		for (int x = 0; x < arrangedCompareData.length; x++) {
+			for (int z = 0; z < data.length; z++) {
+				if (data[z].id == arrangedCompareData[x].id) {
+					System.out.print(data[z].id + "    " + data[z].deadline + "      " + data[z].profit
+							+ "      " + data[z].startTime + "     " + data[z].endTime);
+				}
+			}
+			System.out.println();
+		}
+
 	}
 }
--- src/main/Test.java
+++ src/main/Test.java
@@ -1,6 +1,6 @@
 /** 
  * Assumption:
- * There are a total of 100 tasks are available for employees to do in a month (30days)
+ * There are a total of 100 tasks are available for employees to do in a month (31days)
  * The tasks will be distributed among 10 employees and will work together to complete the tasks
  * Eg: A task's Start Time is on 1/7/2019 and the End Time is on 30/7/2019. 
  * 	  The deadline for this task will be 30
@@ -12,10 +12,12 @@ public class Test {
 	public static void main(String[] args) {
 
 	    Random rand = new Random();
-		final int nObject = 20;
+		final int nObject = 100;
+		final int finalJob = 30;
 		final int n = 0;
 		Data[] data = new Data[nObject];
 		CompareData[] newData = new CompareData[nObject];
+		CompareData[] arrangedCompareData = new CompareData[finalJob];
 		Algorithm algo = new Algorithm();
         
 		// data[0] = new Data('a', 2, 70, 100, 200); 
@@ -30,18 +32,18 @@ public class Test {
 			data[x] = new Data(Integer.toString(x + 1), endTime, rand.nextInt(100) + 1, startTime, endTime);
 		}
 		System.out.println("ID  "+"Deadline "+"Profit "+"StartTime "+"EndTime");
-		algo.printJobScheduling(ValidateData(data, newData,rand, n), nObject, data);
-        
+		algo.MaxProfitJobList(ValidateData(data, newData,rand, n), data, arrangedCompareData);
+
 
 	}
 	public static CompareData[] ValidateData(Data data[], CompareData newData[], Random rand, int n) {
 		if (n <= data.length - 1) {
+			data[n].id = Integer.toString(n + 1);
+			data[n].profit = rand.nextInt(100) + 1;
 			data[n].startTime = rand.nextInt(30) + 1;
 			data[n].endTime = rand.nextInt(30) + 1;
 			data[n].deadline = data[n].endTime;
-			data[n].id = Integer.toString(n + 1);
-			data[n].profit = rand.nextInt(100) + 1;
-
+			
 			if (data[n].startTime > data[n].endTime) {
 				return ValidateData(data, newData, rand, n);
 			} else {
@@ -52,5 +54,4 @@ public class Test {
 		}
 		return newData;
 	}
-
 }
