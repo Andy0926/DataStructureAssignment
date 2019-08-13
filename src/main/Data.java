fb97ffed986ccb24cd96d5ba94f156502a6e05a0
Tan Andy <tanandy2011@hotmail.com>
2019-08-13 19:17:58 +0800

    Recover file

---
+++ src/main/Data.java
@@ -0,0 +1,19 @@
+package main;
+
+public  class Data { 
+    String id; 
+    int deadline; 
+    int profit; 
+    int startTime;
+    int endTime;
+
+    // Constructor 
+    Data(String id, int deadline, int profit, int startTime, int endTime) 
+    { 
+        this.id = id; 
+        this.deadline = deadline; 
+        this.profit = profit; 
+        this.startTime = startTime;
+        this.endTime = endTime;
+    } 
+} 
